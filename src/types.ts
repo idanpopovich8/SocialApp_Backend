42e7f9d639ac01c9470edb9207c7ebb4d5f5df12
AmitMichael12 <amit.michael100@gmail.com>
2026-04-24 16:02:51 +0300

    final fixes

--- src/types.ts
+++ src/types.ts
@@ -8,6 +8,8 @@ export interface IUser {
   password: string;
   image?: string;
   refreshTokens?: string[];
+  onlineStatus?: 'online' | 'away' | 'offline';
+  lastSeen?: Date;
 }
 
 // 2. Like Interface
